cef555844137f185b52437df1b46b3db20ad6deb
aplaxco1 <aplaxco@ucsc.edu>
2023-10-19 11:49:13 -0700

    player respawns after they fall

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -25,25 +25,32 @@ public class PlayerMovement : MonoBehaviour
     public float jumpCooldown;
     public float airMultiplier;
     public KeyCode jumpKey = KeyCode.Space;
-    
+
     bool canJump = true;
+
+    Vector3 spawnPoint;
 
     // Start is called before the first frame update
     void Start()
     {
         //grounded = true;
         rb = GetComponent<Rigidbody>();
-        rb.freezeRotation = true;   
+        rb.freezeRotation = true;
+        spawnPoint = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
         if (isOnGround()) { rb.drag = groundDrag; }
         PlayerInput();
         MovePlayer();
         SpeedControl();
+        if (transform.position.y <= -5.0f)
+        {
+            transform.position = spawnPoint;
+        }
     }
 
 
@@ -52,7 +59,8 @@ public class PlayerMovement : MonoBehaviour
         horizontalInput = Input.GetAxisRaw("Horizontal");
         verticalInput = Input.GetAxisRaw("Vertical");
 
-        if (Input.GetKey(jumpKey) && canJump && isOnGround()) {
+        if (Input.GetKey(jumpKey) && canJump && isOnGround())
+        {
             canJump = false;
             Jump();
             Invoke(nameof(ResetJump), jumpCooldown);
@@ -63,20 +71,23 @@ public class PlayerMovement : MonoBehaviour
     {
         moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
 
-        if (isOnGround()){
-            rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);   
+        if (isOnGround())
+        {
+            rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
         }
-        else {
-            rb.AddForce(moveDirection.normalized * moveSpeed * 10f * airMultiplier, ForceMode.Force);   
+        else
+        {
+            rb.AddForce(moveDirection.normalized * moveSpeed * 10f * airMultiplier, ForceMode.Force);
 
         }
     }
 
-    private void SpeedControl() 
+    private void SpeedControl()
     {
         Vector3 flatVelocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
 
-        if (flatVelocity.magnitude > moveSpeed) {
+        if (flatVelocity.magnitude > moveSpeed)
+        {
             Vector3 limitedVelocity = flatVelocity.normalized * moveSpeed;
             rb.velocity = new Vector3(limitedVelocity.x, rb.velocity.y, limitedVelocity.z);
         }
@@ -88,13 +99,13 @@ public class PlayerMovement : MonoBehaviour
         return Physics.Raycast(transform.position, Vector3.down, distToGround + 0.1f);
     }
 
-    private void Jump() 
+    private void Jump()
     {
         rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
         rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
     }
 
-    private void ResetJump() 
+    private void ResetJump()
     {
         canJump = true;
     }
