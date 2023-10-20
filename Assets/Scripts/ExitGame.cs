c5e3a1cfc7a142cd94eaa286af06d474a186f87a
aplaxco1 <aplaxco@ucsc.edu>
2023-10-19 17:14:41 -0700

    game exits when you reach end

---
+++ Assets/Scripts/ExitGame.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ExitGame : MonoBehaviour
+{
+
+    public RawImage blackScreen;
+    public float fadeSpeed;
+    Color screenColor;
+    private float fadeAmount;
+    bool startFade = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (startFade)
+        {
+            screenColor = blackScreen.color;
+            if (blackScreen.GetComponent<RawImage>().color.a < 1)
+            {
+                fadeAmount = screenColor.a + (fadeSpeed * Time.deltaTime);
+                screenColor = new Color(screenColor.r, screenColor.g, screenColor.b, fadeAmount);
+                blackScreen.color = screenColor;
+            }
+            else
+            {
+                Application.Quit();
+            }
+
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            if (!startFade)
+            {
+                startFade = true;
+            }
+        }
+    }
+}
